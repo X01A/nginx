b86d5f92d566533da7bf14d8d256ecbcbaad16c0
Indexyz <jiduye@gmail.com>
2020-02-21 20:53:02 +0800

    feat(platform): add archlinux support

---
+++ tests/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+export ROLE_NAME="nginx"
+
+docker run \
+    --rm --privileged \
+    -v /lib/modules/:/lib/modules/ \
+    -v $(pwd):/etc/ansible/roles/$ROLE_NAME/ \
+    indexyz/ansible:$1 \
+    -i /etc/ansible/roles/$ROLE_NAME/tests/inventory \
+    /etc/ansible/roles/$ROLE_NAME/tests/test.yml
+
