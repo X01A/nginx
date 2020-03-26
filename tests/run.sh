5827c4ac47b066f9e3ee026017579e272e0fddf5
Indexyz <jiduye@gmail.com>
2020-03-26 18:58:36 +0800

    feat: add verbose when run test

--- tests/run.sh
+++ tests/run.sh
@@ -9,5 +9,5 @@ docker run \
     -v $(pwd):/etc/ansible/roles/$ROLE_NAME/ \
     indexyz/ansible:$1 \
     -i /etc/ansible/roles/$ROLE_NAME/tests/inventory \
-    /etc/ansible/roles/$ROLE_NAME/tests/test.yml
+    /etc/ansible/roles/$ROLE_NAME/tests/test.yml -v
 
